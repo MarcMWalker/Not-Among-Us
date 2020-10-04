ea6f6c79de5d2f1fb7bad3ef01b5094834324088
Marc Walker <dev.marcwalker@gmail.com>
2020-10-04 17:51:21 +0100

    Replaced 2D Movement of map with 3D
    Also starting to incorporate fog of way but sttill in testing phase.

---
+++ Not Among Us/Assets/Scripts/Player3D_Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3D_Movement : MonoBehaviour
+{
+
+    public float speed;
+    bool faceRight = true;
+    Vector3 velocity;
+    Camera viewCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        viewCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        gameObject.transform.Translate(direction.normalized * Time.deltaTime * speed);
+        Flip(horizontal);
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !faceRight || horizontal < 0 && faceRight)
+        {
+            faceRight = !faceRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
